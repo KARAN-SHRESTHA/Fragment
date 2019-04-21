07c70c95e5e3611668220b3f2dae676c6b138c9f
Softwarica College Lab <softwarica@softwarica.com>
2019-04-21 11:28:33 +0545

    area created.

--- app/src/main/java/com/n/fragment/SecondFragment.java
+++ app/src/main/java/com/n/fragment/SecondFragment.java
@@ -6,13 +6,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SecondFragment extends Fragment {
+public class SecondFragment extends Fragment implements View.OnClickListener {
 
+
+    private EditText Radius;
+    private Button btnArea;
+    private TextView out;
 
     public SecondFragment() {
         // Required empty public constructor
@@ -22,8 +30,22 @@ public class SecondFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        View view = inflater.inflate(R.layout.fragment_second,container,false);
+
+        Radius = view.findViewById(R.id.Radius);
+        btnArea = view.findViewById(R.id.btnArea);
+        out = view.findViewById(R.id.out);
+        btnArea.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+
+        float radius = Float.parseFloat(Radius.getText().toString());
+        float area = 3.14f * radius * radius;
+        out.setText(Float.toString(area));
+        Toast.makeText(getActivity(), "area is" + area , Toast.LENGTH_SHORT).show();
+
+    }
 }
