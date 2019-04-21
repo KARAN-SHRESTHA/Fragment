7162527166ad016d4029200d787e8e4356088cee
Softwarica College Lab <softwarica@softwarica.com>
2019-04-21 11:18:13 +0545

    sum created

--- app/src/main/java/com/n/fragment/FirstFragment.java
+++ app/src/main/java/com/n/fragment/FirstFragment.java
@@ -1,18 +1,27 @@
 package com.n.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FirstFragment extends Fragment {
+public class FirstFragment extends Fragment implements View.OnClickListener{
 
+
+    private Button btnSum;
+    private EditText Sum1, Sum2;
+    private TextView result;
 
     public FirstFragment() {
         // Required empty public constructor
@@ -22,8 +31,31 @@ public class FirstFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+
+        Sum1 = view.findViewById(R.id.Sum1);
+        Sum2 = view.findViewById(R.id.Sum2);
+        btnSum = view.findViewById(R.id.btnSum);
+        result = view.findViewById(R.id.result);
+
+        btnSum.setOnClickListener(this);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+
+
+        int first = Integer.parseInt(Sum1.getText().toString());
+        int second = Integer.parseInt(Sum2.getText().toString());
+
+        int total = first + second;
+
+        Toast.makeText(getActivity(), "Sum is" + total , Toast.LENGTH_SHORT).show();
+        result.setText(Integer.toString(total));
+    }
 }
--- app/src/main/java/com/n/fragment/MainActivity.java
+++ app/src/main/java/com/n/fragment/MainActivity.java
@@ -1,10 +1,16 @@
 package com.n.fragment;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button btn;
+    private Boolean status = true;
 
 
 
@@ -13,8 +19,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(this);
 
 
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+
+        if(status)
+        {
+
+            FirstFragment ff = new FirstFragment();
+            ft.add(R.id.RelativeContainer,ff);
+            ft.replace(R.id.RelativeContainer,ff);
+            ft.addToBackStack(null);
+            ft.commit();
+            btn.setText("load second fragment");
+            status=false;
+
+        }
+        else
+        {
+            SecondFragment sf = new SecondFragment();
+            ft.add(R.id.RelativeContainer,sf);
+            ft.replace(R.id.RelativeContainer,sf);
+            ft.addToBackStack(null);
+            ft.commit();
+            btn.setText("load first fragment");
+            status=true;
+        }
 
     }
 }
